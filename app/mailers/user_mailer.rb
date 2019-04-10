4419740f918f346750c73a66197e2411e17d76aa
alan <alan.tellecom@gmail.com>
2019-04-10 21:11:14 +0000

    adicionando elio email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
     def reserva_email(reserva)
         @reserva_info = reserva
 
-        mail(subject: 'Confirmação de reserva Saracutacus Hostel', to: @reserva_info.user.email, from: 'alan@saracutacus.site') do |format|
+        mail(subject: 'Confirmação de reserva Saracutacus Hostel', to: "eliomf.rj@gmail.com; #{@reserva_info.user.email}", from: 'alan@saracutacus.site') do |format|
           format.text
           format.html
         end
