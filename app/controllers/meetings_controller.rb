5cfb862ee604747ee55dc0f94d71f5383c9f0565
Alan <alan.tellecom@gmail.com>
2019-04-22 23:27:53 -0300

    adicionando escopo ao CRUD meetings

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -77,7 +77,7 @@ class MeetingsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_meeting
-      @meeting = Meeting.find(params[:id])
+      @meeting = current_user.meetings.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
